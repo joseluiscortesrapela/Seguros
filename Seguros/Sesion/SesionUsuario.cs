217365b22dec5d83137be4f07cd0ed5f1b85a576
Jose Luis Cortes Rapela <joseluiscortesrapela@hotmail.com>
2024-02-15 20:22:40 +0100

    Add project files.

---
+++ Seguros/Sesion/SesionUsuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Seguros
+{
+    internal class SesionUsuario
+    {
+
+        private static int id;
+        private static String usuario;
+        private static String contraseña;
+        private static int puntuacion;
+        private static String tipo;
+
+
+        public static int Id { get => id; set => id = value; }
+        public static string Usuario { get => usuario; set => usuario = value; }
+        public static string Contraseña { get => contraseña; set => contraseña = value; }
+        public static int Puntuacion { get => puntuacion; set => puntuacion = value; }
+        public static string Tipo { get => tipo; set => tipo = value; }
+    }
+}
